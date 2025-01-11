68ad6681dc5e0cfe74aa81c2b3969ef6495a46f8
Alix <alexandraaa.yz@outlook.com>
2025-01-10 22:57:31 -0800

    add function strsplit

---
+++ R/strsplit1.R
@@ -0,0 +1,14 @@
+#' A simple wrapper for strsplit that extracts the first element of the result.
+#'
+#' @param x A character string to be split.
+#' @param split A character string used as the delimiter.
+#'
+#' @return A character vector resulting from splitting `x` by `split`.
+#' @examples
+#' strsplit1("alfa,bravo,charlie,delta")
+#' Returns c("alfa", "bravo", "charlie", "delta")
+#'
+
+strsplit1 <- function(x, split) {
+  strsplit(x, split = split)[[1]]
+}
